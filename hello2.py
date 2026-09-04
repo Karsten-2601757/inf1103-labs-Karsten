0360637af533fd3d4d441b5346c215231ef3ee0b
Karsten <2601757@sit.singaporetech.edu.sg>
2026-09-04 18:04:27 +0800

    added if statement

--- hello2.py
+++ hello2.py
@@ -3,11 +3,16 @@ print('Welcome here')
 print('My first post!')
 print('==========================')
 
-username = 'cool creator'
-bio = 'Fun blogger'
-followers = 100
+username = input('Enter username:')
+age = int(input('Enter age:'))
+category = input('Enter content category:')
 
-print('username:', username)
-print('Bio:', bio)
-print('Followers:', followers)
+print('\nInstagram profile')
+print('==========================')
+print('Username:', username)
+print('Age:', age)
+print('Catergory:', category)
 
+if age>40 and category == "fun":
+    print("You are old, what is fun for you?")
+    
