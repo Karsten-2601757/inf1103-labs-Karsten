6749eaae7b3041c7987e9b21cd914307de4d0934
Karsten <2601757@sit.singaporetech.edu.sg>
2026-09-04 17:50:15 +0800

    added username, bio and followers

---
+++ hello2.py
@@ -0,0 +1,12 @@
+print('==========================')
+print('Welcome here')
+print('My first post!')
+print('==========================')
+
+username = 'cool creator'
+bio = 'Fun blogger'
+followers = 100
+
+print('username:', username)
+print('Bio:', bio)
+print('Followers:', followers)
