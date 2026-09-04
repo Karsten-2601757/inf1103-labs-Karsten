32bbf0e23ff2b2e3fd1b16246b077b0bd940e725
Karsten <2601757@sit.singaporetech.edu.sg>
2026-09-04 17:17:31 +0800

    first post

---
+++ hello.py
@@ -0,0 +1,18 @@
+print('==========================')
+print('Welcome here')
+print('My first post!')
+print('==========================')
+
+username = 'karsten'
+bio = 'fun blogger'
+followers = 100
+
+followers += 50
+print('Day 1 followers: ', followers)
+
+followers += 50
+print('Day 2 followers: ', followers)
+
+followers += 50
+print('Day 3 followers: ', followers)
+
