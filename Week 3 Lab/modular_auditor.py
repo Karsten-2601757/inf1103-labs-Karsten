b6484459eaea3fc1b2a5681571ccfac9f740cd6c
Karsten-2601757 <2601757@sit.singaporetech.edu.sg>
2026-09-18 18:06:30 +0800

    Added process_delivery and calculate tax

--- Week 3 Lab/modular_auditor.py
+++ Week 3 Lab/modular_auditor.py
@@ -5,11 +5,19 @@ fails = 0
 
 def get_valid_input():
     user_input = input("Please enter a valid integer (Type quit to exit):")
-    if user_input.lower == "quit": #if user types in quit, the code will "Break"
+    if user_input.lower == "quit": 
         return "quit"
-        break 
+     
     elif user_input.isdigit:
         return int(user_input)
+    
     else:
         print("Please input a valid non-negative integer.")
         return "none"
+
+def process_delivery(current_total, new_value):
+    return current_total + new_value
+
+def calculate_tax(amount):
+    return amount * 0.1
+
