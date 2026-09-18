2a92290cf76c48f879d8dc8da052fc7b8fa8ca70
Karsten-2601757 <2601757@sit.singaporetech.edu.sg>
2026-09-18 18:00:29 +0800

    Added get_valid_input()

--- Week 3 Lab/modular_auditor.py
+++ Week 3 Lab/modular_auditor.py
@@ -1,2 +1,15 @@
 # auditor.py
 # Smart Inventory Auditor
+inventory = 0 
+fails = 0
+
+def get_valid_input():
+    user_input = input("Please enter a valid integer (Type quit to exit):")
+    if user_input.lower == "quit": #if user types in quit, the code will "Break"
+        return "quit"
+        break 
+    elif user_input.isdigit:
+        return int(user_input)
+    else:
+        print("Please input a valid non-negative integer.")
+        return "none"
